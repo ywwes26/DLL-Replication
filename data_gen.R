ec375d5c63a8ff3ff9c9385586d82f9539bc2aa7
ywwes26 <49973874+ywwes26@users.noreply.github.com>
2022-08-31 22:11:16 -0400

    Update data_gen.R

--- data_gen.R
+++ data_gen.R
@@ -82,7 +82,7 @@ data_gen = function(n=1000, p=1500, setting="1", approx_sparse = FALSE, df = NUL
     y = g1(X[,1]) + g2(X[,2]) + g3(X[,3]) + g4(X[,4]) + g5(X[,5]) + g6(X[,6]) + e
   }
   
-  if (setting = "nonlinear_treatment") {
+  if (setting == "nonlinear_treatment") {
     mean.x = -0.25
     X = mvrnorm(n,rep(mean.x,p),Sigma = Cov_Matrix)
     D = -0.5*exp(-X[,1]/2) + 0.5*sin(X[,2]) + 0.25*X[,3]^2 - 0.5*X[,4] - 0.25*X[,5]^2 + 0.5*cos(X[,6]) - 0.25*exp(X[,7]/2) + 0.25*X[,8] + rnorm(n,sd=0.5)
