c82a159fe420e7ff79d4a5f849e3115a1892e9b6
ywwes26 <49973874+ywwes26@users.noreply.github.com>
2022-08-31 22:08:05 -0400

    Update data_gen.R

--- data_gen.R
+++ data_gen.R
@@ -54,9 +54,8 @@ data_gen = function(n=1000, p=1500, setting="1", approx_sparse = FALSE, df = NUL
     
     mean.x = -0.25
     sd.x = 1
-    # CDF and PDF of X
+    # CDF of X
     p.x = function(x) pnorm(x,mean = mean.x, sd = sd.x)
-    d.x = function(x) dnorm(x,mean = mean.x, sd = sd.x)
     X = mvrnorm(n,rep(mean.x,p_dx),Sigma = Cov_Matrix)
     Z = apply(X,2,p.x)
     e = rnorm(n,sd=1)
@@ -67,9 +66,8 @@ data_gen = function(n=1000, p=1500, setting="1", approx_sparse = FALSE, df = NUL
   if (setting == "3") {
     mean.x = -0.25
     sd.x = 1
-    # CDF and PDF of X
+    # CDF of X
     p.x = function(x) pnorm(x,mean = mean.x, sd = sd.x)
-    d.x = function(x) dnorm(x,mean = mean.x, sd = sd.x)
     X = mvrnorm(n,rep(mean.x,p_dx),Sigma = Cov_Matrix)
     X = (apply(X,2,p.x)-0.5)*5 # create correlated Uniform X on (-2.5,2.5)
     e = rnorm(n,sd=1)
@@ -79,6 +77,16 @@ data_gen = function(n=1000, p=1500, setting="1", approx_sparse = FALSE, df = NUL
   
   if (setting == "4") {
     X = rmvt(n,Cov_Matrix*(df-2)/df,df=df)
+    e = rnorm(n,sd=1)
+    # generate response Y
+    y = g1(X[,1]) + g2(X[,2]) + g3(X[,3]) + g4(X[,4]) + g5(X[,5]) + g6(X[,6]) + e
+  }
+  
+  if (setting = "nonlinear_treatment") {
+    mean.x = -0.25
+    X = mvrnorm(n,rep(mean.x,p),Sigma = Cov_Matrix)
+    D = -0.5*exp(-X[,1]/2) + 0.5*sin(X[,2]) + 0.25*X[,3]^2 - 0.5*X[,4] - 0.25*X[,5]^2 + 0.5*cos(X[,6]) - 0.25*exp(X[,7]/2) + 0.25*X[,8] + rnorm(n,sd=0.5)
+    X = cbind(D,X)
     e = rnorm(n,sd=1)
     # generate response Y
     y = g1(X[,1]) + g2(X[,2]) + g3(X[,3]) + g4(X[,4]) + g5(X[,5]) + g6(X[,6]) + e
